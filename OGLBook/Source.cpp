aee88952a499a1379e73c21b7ac65a26df58a11d
MORCHIK1 <MORCHIK1>
2025-06-12 17:02:19 +0300

    Added cube, removed transformation matrix, added cube rotation and depth test

--- OGLBook/Source.cpp
+++ OGLBook/Source.cpp
@@ -43,30 +43,52 @@ int main()
   Texture ourFaceTexture("awesomeface.png");
   
   float vertices[] = {
-    //Positions      //Textures
-    0.5f, -0.5f, 0.0f, 1.f, 0.f, //bottom right red color 
-   -0.5f, -0.5f, 0.0f, 0.f, 0.f, //bottom left green color
-    0.5f,  0.5f, 0.0f, 1.f, 1.f, //top rigt blue color
-   -0.5f,  0.5f, 0.0f, 0.f, 1.f  //top left yellow color
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f, 0.0f, 1.0f
   };
 
-  unsigned int indices[] = {
-    0,1,2,
-    1,2,3
-  };
-
-  unsigned int VAO, VBO, EBO;
+  unsigned int VAO, VBO;
 
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
@@ -83,13 +105,14 @@ int main()
   ourShader.setInt("texture1", 0);
   ourShader.setInt("texture2", 1);
 
+  glEnable(GL_DEPTH_TEST);
 
   while (!glfwWindowShouldClose(window))
   {
     processInput(window);
 
     glClearColor(0.2f, 0.2f, 0.2f, 1.f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     //Using all textures and shaders
     ourContainerTexture.use(0); 
@@ -97,19 +120,21 @@ int main()
     ourShader.use();
     
     //Creating transform matrix for rotation and translation
+    /*
     glm::mat4 trans = glm::mat4(1.f);
 
     trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.f));
     trans = glm::rotate(trans, static_cast<float>(glfwGetTime()), glm::vec3(0.f, 0.f, 1.f));
 
     ourShader.setMat4("transform", trans);
-    
+    */
+
     //Creating model, view, projection matrices for 3D and sending it to vertexShader
     glm::mat4 model      = glm::mat4(1.f);
     glm::mat4 view       = glm::mat4(1.f);
     glm::mat4 projection = glm::mat4(1.f);
 
-    model = glm::rotate(model, glm::radians(-55.f), glm::vec3(1.f, 0.f, 0.f)); //Rotating our object
+    model = glm::rotate(model, static_cast<float>(glfwGetTime()) * glm::radians(50.f), glm::vec3(1.f, 1.f, 0.f)); //Rotating our object
     view = glm::translate(view, glm::vec3(0.f, 0.f, -3.f));
     projection = glm::perspective(glm::radians(55.f), SCR_WIDTH / SCR_HEIGHT, 0.1f, 100.f); //fov, aspect ratio, near distance, far distance
 
@@ -119,7 +144,7 @@ int main()
 
     glBindVertexArray(VAO);
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -127,7 +152,6 @@ int main()
 
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
-  glDeleteBuffers(1, &EBO);
 
   glfwTerminate();
   return 0;
